6cf5446946ffed2f325db900b67646b0308916bb
Chen <chennsh5@mail2.sysu.edu.cn>
2017-04-04 18:23:52 +0800

    first commit

---
+++ lab1_code/Experiment1/app/src/main/java/com/example/chen/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.chen.myapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Chen on 2016/9/14.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
